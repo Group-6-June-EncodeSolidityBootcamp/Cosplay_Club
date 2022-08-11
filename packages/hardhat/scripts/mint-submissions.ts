b46b8aff8629da1f3e54940177c01018080c4e0e
AbdulSamad <47020656+4samad@users.noreply.github.com>
2022-08-11 17:21:15 +0400

    create script mint nfts

---
+++ packages/hardhat/scripts/mint-submissions.ts
@@ -0,0 +1,45 @@
+import { ethers } from "hardhat";
+
+const CONTRACT_ADDRESS = "0xa05AD8C8FA9252626bcA667107E4e8eA6581bff0";
+const TO_MINT_TOKEN_URI = "https://starwars-cosplayclub.glitch.me/nft/token";
+const NO_OF_TOKENS = 30;
+
+async function main() {
+
+  const [minter] = await ethers.getSigners();
+
+  console.log("Minting with the account:", minter.address);
+
+  console.log("Account balance:", (await minter.getBalance()).toString());
+
+  const address = CONTRACT_ADDRESS;
+  const Contract = await ethers.getContractFactory('Contest');
+  const contract = await Contract.attach(address);
+
+  console.log("Contract address:", contract.address);
+
+  // Get total tokens
+  let totalSubmissions = await contract.totalSubmissions();
+  console.log('Total Tokens was ', totalSubmissions.toString());
+  
+  // Send transactions to mint
+  for(let i = 0; i < NO_OF_TOKENS; i +=1) {
+    const toMintTokenURI = TO_MINT_TOKEN_URI + i;
+    const tx = await contract.submitItem(toMintTokenURI);
+    await tx.wait()
+    console.log(`Minted ${toMintTokenURI} to ${minter.address}`);
+    console.log(`Transaction hash: ${tx.hash}`);
+  }
+
+
+  // Get total tokens again
+  totalSubmissions = await contract.totalSubmissions();
+  console.log('Total Tokens is now ', totalSubmissions.toString());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
