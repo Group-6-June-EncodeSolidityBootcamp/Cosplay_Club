479abe8c3192c6b3446af2f089c18fc03804b3c5
AbdulSamad <47020656+4samad@users.noreply.github.com>
2022-09-16 23:42:14 +0400

    fix worldid (wip)

--- packages/frontend/src/WorldID.jsx
+++ packages/frontend/src/WorldID.jsx
@@ -1,6 +1,8 @@
-import logo from "./logo.svg";
 import "./WorldID.css";
 import { WorldIDWidget } from "@worldcoin/id";
+
+import contractABI from "./assets/worldIDABI";
+const CONTRACT_ADDRESS = "0xa05AD8C8FA9252626bcA667107E4e8eA6581bff0";
 
 const widgetProps = {
   actionId: "wid_staging_7919c463dab9a1b7ba9d4e836d4a6018",
@@ -10,7 +12,10 @@ const widgetProps = {
   signalDescription: "Proof of Personhood",
   theme: "light",
   debug: true, // DO NOT SET TO `true` IN PRODUCTION
-  onSuccess: (result) => console.log(result),
+  onSuccess: (result) => {
+    console.log(result);
+    // call contract.verifyAndExecute(...result,tokenId)
+  },
   onError: ({ code, detail }) => console.log({ code, detail }),
   onInitSuccess: () => console.log("Init successful"),
   onInitError: (error) => console.log("Error while initialization World ID", error),
@@ -20,8 +25,7 @@ export function WorldID() {
   return (
     <div className="worldIDPage">
       <header className="worldIDPage-header">
-        <img src={logo} className="worldIDPage-logo" alt="logo" />
-        <p>World ID</p>
+        <p>Verify World ID to submit Vote:</p>
         {/* World ID component below */}
         <WorldIDWidget {...widgetProps} />
       </header>
